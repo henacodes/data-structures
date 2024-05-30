e1d296aff4424d0f486934700789524f34907d2a
Henok Ayenew <henacodes@gmail.com>
2024-05-30 09:39:19 +0300

    Hash map added

---
+++ src/main/kotlin/HashTable.kt
@@ -0,0 +1,45 @@
+class HashTable<T : Any> {
+
+    var table = mutableListOf<MutableList<MutableList<Any>>>(
+        mutableListOf(),
+        mutableListOf(),
+        mutableListOf(),
+        mutableListOf(),
+        mutableListOf(),
+        mutableListOf(),
+        mutableListOf()
+    );
+
+    var limit = 6
+
+
+    public fun hash(key: String): Int {
+        var hash = 0;
+        for (i in 0..key.length -1) {
+            hash += (hash + key.get(i).code * 23)
+        };
+        return hash  % limit
+    }
+
+    public fun set(key: String, value: T): MutableList<MutableList<MutableList<Any>>>{
+        val index = hash(key)
+
+        table[index].add(mutableListOf(key, value))
+        return table
+    }
+
+
+    public fun item(key: String): Any? {
+        var index = hash(key)
+        if (index > limit ){
+            return null
+        }
+        val elements = table[index]
+        val result =  elements.filter { el -> el[0] == key}[0][1]
+        return result
+
+    }
+
+}
+
+
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,36 +1,19 @@
 fun main(args: Array<String>) {
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
 
-    var newStack = Stack<Int>(1,2,2,4,5);
-    var newQueue = Queue<String>("first order", "second order", "3rd order");
-    var newTree = BinarySearchTree()
+    val table = HashTable<Int>()
 
+    table.set("washers", 50)
+    table.set("lumber", 21320)
+    table.set("anthropic", 3242)
+    table.set("harmony", 5389)
+    println(table.table)
 
+    //println(table.hash("washers"))
+    //println(table.hash("lumber"))
 
-    var newList = LinkedList<Int>()
-
-    newList.append(3);
-    newList.append(4);
-
-    newList.insert(78, 1)
-    println(newList.head!!.next!!.value)
-    println(newList.head!!.next!!.next!!.value)
-    println(newList.find(0))
-    println(newList.find(1))
-    println(newList.find(2))
-    /*newList.append(5);
-    newList.append(6);
-    newList.append(7);
-    println(newList.size)
-    println(newList.head)*/
-
-
-
-
-
+    println(table.item("lumber"))
+    println(table.item("anthropic"))
 
 
 
